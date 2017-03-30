ed2cdda4e7714ea558220acb85fec710df2dc1f7
Eduardo Londero <eduardolonderovieira@gmail.com>
2017-03-30 10:56:03 -0300

    Atualizado pra última versão

--- Citador.plugin.js
+++ Citador.plugin.js
@@ -1,25 +1,15 @@
 //META{"name":"Citador"}*//
 
-const token = "COLOQUE SEU TOKEN AQUI";
-
 var isQuote = false,
 	quoting = false,
 	elem,
 	chanName,
 	serverName,
-	atServerName;
+	atServerName,
+	stringLocal;
 
 class Citador {
 	constructor() {
-		this.defaultToken = "COLOQUE SEU TOKEN AQUI"; // NÃO MUDE ISSO!
-		this.startMsg = "Iniciado";
-		this.tokenLink = "https://github.com/nirewen/Citador/tree/pt#adquirindo-token";
-		this.notSetToken = `Seu token não está definido. Por favor, siga os passos para a definição do token.<br/><a href='${this.tokenLink}' target='_blank'>Guia do Citador: Como definir seu token</a>.`;
-		this.invalidToken = ['Seu token é inválido. Por favor, cheque-o de novo.', '✖ Token inválido'];
-		this.validToken = '✔ Token válido';
-		this.quotingMsg = " CITANDO";
-		this.quoteTooltip = "Citar";
-		this.deleteTooltip = "Excluir";
 		this.closeImg = "https://discordapp.com/assets/14f734d6803726c94b970c3ed80c0864.svg";
 		this.deleteMsgBtnImg = "https://imgh.us/deleteMsgBtnImgHover.svg";
 		this.css = `
@@ -32,19 +22,27 @@ class Citador {
 				color: #fff !important;
 				position :relative;
 				top: -1px;
-				margin-left: 5px;
+				margin: 0 3px 0 5px;
 				text-transform: uppercase;
 				font-size: 10px;
 				padding: 3px 5px;
-				background: rgba(0,0,0,0.4);
+				background: rgb(10, 10, 10);
 				font-family: "Segoe MDL2 Assets", "Whitney";
 				border-radius: 3px;
 				transition: all 200ms ease-in-out;
 				-webkit-transition: all 200ms ease-in-out;
+				opacity: 0.8;
 			}
+
 			.citar-btn.quoting {
-				background: rgba(114, 137, 218, .8);
+				background: #43b581;
 				cursor: default;
+				opacity: 1 !important;
+			}
+			.citar-btn.cant-embed {
+				background: #f04747;
+				cursor: default;
+				opacity: 1 !important;
 			}
 			.quote-close {
 				opacity: .5; 
@@ -61,7 +59,7 @@ class Citador {
 				opacity: 1;
 			}
 			.delete-msg-btn {
-				opacity: .4;
+				opacity: 0;
 				float: right;
 				width: 16px;
 				height: 16px;
@@ -72,7 +70,7 @@ class Citador {
 				-webkit-transition: opacity 200ms ease-in-out;
 			}
 			.delete-msg-btn:hover {
-				opacity: 1;
+				opacity: 1 !important;
 			}`;
 		this.componentToHex = (c) => {
 			var hex = Number(c).toString(16);
@@ -88,51 +86,86 @@ class Citador {
 			quoting      = false;
 			atServerName = '';
 		};
-		this.log = (message, method) => {
-			switch (method) {
-				case "warn":
-					console.warn  ( `[${this.getName()}]`, message );
-					break;
-				case "error":
-					console.error ( `[${this.getName()}]`, message );
-					break;
-				case "debug":
-					console.debug ( `[${this.getName()}]`, message );
-					break;
-				case "info":
-					console.info  ( `[${this.getName()}]`, message );
-					break;
-				default:
-					console.log   ( `[${this.getName()}]`, message );
-					break;
+		this.getInternalInstance = e => e[Object.keys(e).find(k => k.startsWith("__reactInternalInstance"))];
+		this.getOwnerInstance = (e, {include, exclude=["Popout", "Tooltip", "Scroller", "BackgroundFlash"]} = {}) => {
+			if (e === undefined) {
+				return undefined;
 			}
-		};
-	}
-	start() {
-		var self = this;
-		if (token == "" || token == this.defaultToken) {
-			this.stop();
-			return BdApi.getCore().alert(this.getName(), this.notSetToken);
-		}
-		$.ajax({
-			type : "GET",
-			url : `https://discordapp.com/api/guilds/` + $('.guilds .guild-inner:eq(2)').find('a').attr('href').split('/').pop(),
-			headers : {
-				"Authorization": token
-			},
-			success: () => {
-				this.log(self.validToken, "debug");
-			},
-			error: (req) => {
-				if (JSON.parse(req.responseText).message == "401: Unauthorized") {
-					this.stop();
-					BdApi.getCore().alert(self.getName(), self.invalidToken[0]);
-					this.log(self.invalidToken[1], "error");
-				} else { // in case there's no guild, the token will be also valid, isn't it?
-					this.log(self.validToken, "debug");
+			const excluding = include === undefined;
+			const filter = excluding ? exclude : include;
+			function getDisplayName(owner) {
+				const type = owner._currentElement.type;
+				const constructor = owner._instance && owner._instance.constructor;
+				return type.displayName || constructor && constructor.displayName || null;
+			}
+			function classFilter(owner) {
+				const name = getDisplayName(owner);
+				return (name !== null && !!(filter.includes(name) ^ excluding));
+			}
+
+			for (let prev, curr = this.getInternalInstance(e); !_.isNil(curr); prev = curr, curr = curr._hostParent) {
+				if (prev !== undefined && !_.isNil(curr._renderedChildren)) {
+					let owner = Object.values(curr._renderedChildren)
+						.find(v => !_.isNil(v._instance) && v.getHostNode() === prev.getHostNode());
+					if (!_.isNil(owner) && classFilter(owner)) {
+						return owner._instance;
+					}
+				}
+
+				if (_.isNil(curr._currentElement)) {
+					continue;
+				}
+				let owner = curr._currentElement._owner;
+				if (!_.isNil(owner) && classFilter(owner)) {
+					return owner._instance;
 				}
 			}
-		});
+
+			return null;
+		}
+	}
+	log(message, method) {
+		switch (method) {
+			case "warn":
+				console.warn  ( `[${this.getName()}]`, message );
+				break;
+			case "error":
+				console.error ( `[${this.getName()}]`, message );
+				break;
+			case "debug":
+				console.debug ( `[${this.getName()}]`, message );
+				break;
+			case "info":
+				console.info  ( `[${this.getName()}]`, message );
+				break;
+			default:
+				console.log   ( `[${this.getName()}]`, message );
+				break;
+		}
+	}
+	start() {
+		switch(navigator.language) {
+			case 'pt-BR':
+				stringLocal = {
+					startMsg: "Iniciado",
+					quoteTooltip: "Citar",
+					deleteTooltip: "Excluir",
+					noPermTooltip: "Sem permissão para citar",
+					attachment: "Anexo"
+				};
+				break;
+			case 'en-US':
+				stringLocal = {
+					startMsg: "Started",
+					quoteTooltip: "Quote",
+					deleteTooltip: "Delete",
+					noPermTooltip: "No permission to quote",
+					attachment: "Attachment"
+				}
+				break;
+		}
+		$('body').append('<iframe class="citador-token-grabber">');
+		var self = this;
 		this.attachParser();
 		BdApi.injectCSS("citador-css", this.css);
 		
@@ -144,12 +177,17 @@ class Citador {
 					citarBtn       = '<span class="citar-btn"></span>',
 					closeBtn       = '<div class="quote-close"></div>',
 					deleteMsgBtn   = '<div class="delete-msg-btn"></div>',
-					quoteTooltip   = $("<div>").append(self.quoteTooltip).addClass("tooltip tooltip-top tooltip-normal citador"),
-					deleteTooltip  = $("<div>").append(self.deleteTooltip).addClass("tooltip tooltip-top tooltip-normal citador");
+					quoteTooltip   = $("<div>").append(stringLocal.quoteTooltip).addClass("tooltip tooltip-top tooltip-normal citador"),
+					deleteTooltip  = $("<div>").append(stringLocal.deleteTooltip).addClass("tooltip tooltip-top tooltip-normal citador"),
+					noPermTooltip  = $("<div>").append(stringLocal.noPermTooltip).addClass("tooltip tooltip-top tooltip-error citador");
 				
 				todasMensagens.on('mouseover', function() {
-					if (nomeData.find('.citar-btn').length == 0) {
-						$(this).find(nomeData).append(citarBtn);
+					if ($(this).find('.citar-btn').length == 0) {
+						if (todasMensagens.hasClass('compact')) {
+							$(this).find('.timestamp').first().prepend(citarBtn);
+						} else {
+							$(this).find(nomeData).append(citarBtn);
+						}
 						$(this).find('.citar-btn')
 							.on('mousedown.citador', function() {return false;})
 							.on('mouseover.citador', function() {
@@ -184,13 +222,18 @@ class Citador {
 									elem = $('.quote-msg');
 									
 									$('.quote-msg').find('.citar-btn').toggleClass('quoting');
-									$('.quote-msg').find('.citar-btn').text(self.quotingMsg);
+									$('.quote-msg').find('.citar-btn').text('');
 									
 									$('.quote-msg').find('.embed').each(function() {
 										$(this).closest('.accessory').remove();
 									});
 									
 									$('.quote-msg').find('.markup').each(function() {
+										if ($(this).text() == "" && $(this).closest(".message").find('.accessory').length == 0) {
+											$(this).closest('.message-text').remove();
+										}
+									});
+									$('.quote-msg').find('.message-content').each(function() {
 										if ($(this).text() == "" && $(this).closest(".message").find('.accessory').length == 0) {
 											$(this).closest('.message-text').remove();
 										}
@@ -253,6 +296,36 @@ class Citador {
 											$('.quote-msg').find(".message.first").find(".message-text").append('<div class="markup">' + text + '</div>');
 										}
 									}
+
+									$('.quote-msg').find(".message")
+										.on('mouseover.citador', function() {
+											$(this).find('.delete-msg-btn').fadeTo(10, 0.4);
+										})
+										.on('mouseout.citador', function() {
+											$(this).find('.delete-msg-btn').fadeTo(10, 0);
+										});
+
+									var elemento = document.querySelector(".messages"),
+										channel = self.getOwnerInstance(elemento, {include:["Channel"]}),
+										canEmbed = channel.state.channel.isPrivate() || channel.can(0x4800, {channelId: channel.state.channel.id});
+									
+									if (canEmbed == false) {
+										$('.quote-msg').find('.citar-btn').toggleClass('quoting');
+										$('.quote-msg').find('.citar-btn').toggleClass('cant-embed');
+										$('.quote-msg').find('.citar-btn').text("");
+										$('.quote-msg').find('.citar-btn')
+											.on('mouseover.citador', function() {
+												$(".tooltips").append(noPermTooltip);
+												var position = $(this).offset();
+												position.top -= 30;
+												position.left += $(this).width()/2 - noPermTooltip.width()/2 - 5;
+												noPermTooltip.offset(position);
+												$(this).on("mouseout.citador", function () {
+													$(this).off("mouseout.citador");
+													noPermTooltip.remove();
+												})
+											});
+									}
 								}
 								
 								if (quoting == true) {
@@ -269,13 +342,13 @@ class Citador {
 					}
 				});
 				todasMensagens.on('mouseleave',function() {
-					if (nomeData.find('.citar-btn').length == 1) {
+					if ($(this).find('.citar-btn').length == 1) {
 						$(this).find('.citar-btn').empty().remove();
 					}
 				});
 			}
 		});
-		this.log(this.startMsg, "info");
+		this.log(stringLocal.startMsg, "info");
 	}
 	attachParser() {
 		var el   = $('.channel-textarea textarea'),
@@ -290,9 +363,9 @@ class Citador {
 				if (isQuote == true) {
 					if (e.shiftKey || $('.channel-textarea-autocomplete-inner').length >= 1) return;
 					
-					var color     = $('.quote-msg').find('.user-name').css('color'),
-						user      = $('.quote-msg').find('.user-name').text(),
-						avatarUrl = $('.quote-msg').find('.avatar-large').css('background-image').replace(/.*\s?url\([\'\"]?/, '').replace(/[\'\"]?\).*/, ''),
+					var color     = $('.quote-msg').find('.user-name').first().css('color'),
+						user      = $('.quote-msg').find('.user-name').first().text(),
+						avatarUrl = $('.quote-msg').find('.avatar-large').css('background-image') ? $('.quote-msg').find('.avatar-large').css('background-image').replace(/.*\s?url\([\'\"]?/, '').replace(/[\'\"]?\).*/, '') : '',
 						oldText   = $('.content .channel-textarea textarea').val(),
 						hourpost  = $('.quote-msg').find('.timestamp').text(),
 						quoteMsg  = $('.quote-msg').find('.comment'),
@@ -312,11 +385,20 @@ class Citador {
 					quoteMsg.find(    '.emoji'   ).each(function() {$(this).html($(this).attr('alt'));});
 					
 					// definir texto pra citar
-					quoteMsg.find('.markup').each(function() {text += $(this).clone().end().text() + '\n';});
+					if ($('.messages .message-group').hasClass('compact')) {
+						quoteMsg.find('.message-content').each(function() {text += `${$(this).clone().end().text()}\n`;});
+					} else {
+						quoteMsg.find('.markup').each(function() {text += `${$(this).clone().end().text()}\n`;});
+					}
 					
 					// converte a cor do cargo pra hex 
 					color = color.split(/rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/);
 					color = Number('0x' + self.rgbToHex(color[1], color[2], color[3]).toString());
+
+					if ($('.messages .message-group').hasClass('compact')) {
+						$('.quote-msg').find('.timestamp').first().find('i, .citar-btn').remove();
+						hourpost = $('.quote-msg').find('.timestamp').first().text();
+					}
 					
 					// os dados do embed 
 					var data = {
@@ -336,7 +418,7 @@ class Citador {
 							}
 						},
 						chanID = window.location.pathname.split('/').pop();
-					
+
 					// checar se tem alguma imagem na mensagem citada, e adicionar ao embed final
 					if ($('.quote-msg').find('.attachment-image').length >= 1) {
 						data.embed.image.url = $('.quote-msg').find('.attachment-image a').attr('href');
@@ -344,20 +426,20 @@ class Citador {
 					
 					// checar se tem algum arquivo na mensagem citada, e adicionar ao embed final
 					if ($('.quote-msg').find('.attachment').length >= 1) {
-						for (i = 0; i < $('.quote-msg').find('.attachment').length; i++) {
+						for (var i = 0; i < $('.quote-msg').find('.attachment').length; i++) {
 							var value     = $($('.quote-msg').find('.attachment')[i]).find('.attachment-inner a').text(),
 								link      = $($('.quote-msg').find('.attachment')[i]).find('.attachment-inner a').attr('href'),
 								attachNum = i + 1;
-							data.embed.fields.push({name: "Attachment #" + attachNum, value: `📁 [${value}](${link})`});
+							data.embed.fields.push({name: `${stringLocal.attachment} #${attachNum}`, value: `📁 [${value}](${link})`});
 						}
 					}
 					
-					// post do embed final
+					// post do quote final
 					$.ajax({
 						type : "POST",
 						url : `https://discordapp.com/api/channels/${chanID}/messages`,
 						headers : {
-							"Authorization": token
+							"Authorization": $('body').find('iframe')[0].contentWindow.localStorage.token.split('"')[1]
 						},
 						dataType : "json",
 						contentType : "application/json",
@@ -385,27 +467,45 @@ class Citador {
 			}
 		}, false);
 	}
+	deleteEverything() {
+		$(document).off("mouseover.citador");
+		$('.messages .message-group').off('mouseover');
+		$('.messages .message-group').off('mouseleave');
+		BdApi.clearCSS("citador-css");
+		$('.citador-token-grabber').remove();
+	}
 	getName         () { return "Citador";             }
 	getDescription  () { return "Cita alguém no chat"; }
-	getVersion      () { return "1.5.2";               }
+	getVersion      () { return "1.5.1";               }
 	getAuthor       () { return "Nirewen";             }
-	getSettingsPanel() { return '';                    }
-	load            () {};
-	unload() {
-		$(document).off("mouseover.citador");
-		$('.messages .message-group').off('mouseover');
-		$('.messages .message-group').off('mouseleave');
-		BdApi.clearCSS("citador-css");
-	}
-	stop() {
-		$(document).off("mouseover.citador");
-		$('.messages .message-group').off('mouseover');
-		$('.messages .message-group').off('mouseleave');
-		BdApi.clearCSS("citador-css");
-	}
+	getSettingsPanel() { return "";                    }
+	load            () { return "";                    }
+	unload          () {   this.deleteEverything();    }
+	stop            () {   this.deleteEverything();    }
 	onSwitch() {
 		this.attachParser();
 		if (isQuote == true) {
+			var elemento = document.querySelector(".messages"),
+				channel = this.getOwnerInstance(elemento, {include:["Channel"]}),
+				canEmbed = channel.state.channel.isPrivate() || channel.can(0x4800, {channelId: channel.state.channel.id});
+			
+			if (canEmbed == false) {
+				$('.quote-msg').find('.citar-btn').toggleClass('quoting');
+				$('.quote-msg').find('.citar-btn').toggleClass('cant-embed');
+				$('.quote-msg').find('.citar-btn').text("");
+				$('.quote-msg').find('.citar-btn')
+					.on('mouseover.citador', function() {
+						$(".tooltips").append(noPermTooltip);
+						var position = $(this).offset();
+						position.top -= 30;
+						position.left += $(this).width()/2 - noPermTooltip.width()/2 - 11;
+						noPermTooltip.offset(position);
+						$(this).on("mouseout.citador", function () {
+							$(this).off("mouseout.citador");
+							noPermTooltip.remove();
+						})
+					});
+			}
 			if (serverName !== $('.guild-header header span').text() && serverName !== "") {
 				atServerName = " at " + serverName;
 			} else if (serverName == $('.guild-header header span').text() || serverName == ""){
