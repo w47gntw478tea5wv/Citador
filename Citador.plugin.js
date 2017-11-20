a172f0243f93107aa8ed72c6fce22d9f3426e636
Eduardo Londero <eduardolonderovieira@gmail.com>
2017-11-20 00:44:41 -0200

    Fallback Codeblock quote

--- Citador.plugin.js
+++ Citador.plugin.js
@@ -81,7 +81,10 @@ class Citador {
 		this.EventDispatcher   = PluginUtilities.WebpackModules.findByUniqueProperties(["dispatch"]);
 		this.MainDiscord       = PluginUtilities.WebpackModules.findByUniqueProperties(["ActionTypes"]);
 		this.HistoryUtils      = PluginUtilities.WebpackModules.findByUniqueProperties(['transitionTo', 'replaceWith', 'getHistory']);
+		this.moment            = PluginUtilities.WebpackModules.findByUniqueProperties(['parseZone']);
+	
 		this.patchExternalLinks();
+		
 		$(document).on("mouseover.citador", function(e) {
 			var target = $(e.target);
 			if (target.parents(".message").length > 0) {
@@ -105,11 +108,11 @@ class Citador {
 									
 									self.quoteProps = $.extend(true, {}, ReactUtilities.getOwnerInstance(message[0]).props);
 										
-									if (window.getSelection && window.getSelection().rangeCount > 0) {
+									if (window.getSelection && window.getSelection().rangeCount > 0)
 										range = window.getSelection().getRangeAt(0);
-									} else if (document.selection && document.selection.type !== 'Control') {
+									else if (document.selection && document.selection.type !== 'Control')
 										range = document.selection.createRange();
-									}
+										
 									var thisPost = $(this).closest('.comment');
 									
 									this.createQuote = function() {
@@ -149,8 +152,6 @@ class Citador {
 											.each(function() {
 												new PluginTooltip.Tooltip($(this), self.local.deleteTooltip);
 											});
-										
-										$('.channelTextArea-1HTP3C').focus();
 
 										if (range) {
 											var startPost = $(range.startContainer).closest('.message'),
@@ -191,8 +192,7 @@ class Citador {
 												$(this).find('.delete-msg-btn').fadeTo(5, 0);
 											});
 
-										var canEmbed = channel.isPrivate() || mInstance.can(0x4800, {channelId: channel.id});
-										if (!canEmbed) {
+										if (!self.canEmbed()) {
 											$('.quote-msg').find('.citar-btn.hidden:not(.cant-embed)').toggleClass('hidden cant-embed');
 											new PluginTooltip.Tooltip($('.quote-msg').find('.citar-btn'), self.local.noPermTooltip, 'red');
 										}
@@ -206,140 +206,202 @@ class Citador {
 										$('.channelTextArea-1HTP3C').prepend('<div class="quote-msg"></div>');
 									
 									this.createQuote();
+									$('.channelTextArea-1HTP3C').focus();
 								});
 						}
 					})
 					.on('mouseleave',function() {
-						if ($(this).find('.citar-btn').length == 1) {
+						if ($(this).find('.citar-btn').length == 1)
 							$(this).find('.citar-btn').empty().remove();
-						}
 					});
 			}
 		});
 		this.log(this.local.startMsg, "info");
 	}
 	
+	canEmbed() {
+		const channel = ReactUtilities.getOwnerInstance($(".messages-wrapper")[0]);
+		return channel.props.channel.isPrivate() || channel.can(0x4800, {channelId: channel.props.channel.id});
+	}
+	
 	attachParser() {
-		var el   = $('.channelTextArea-1HTP3C'),
-			self = this;
-			
+		var el   = $('.channelTextArea-1HTP3C');
 		if (el.length == 0) return;
 		
-		this.handleKeypress = function(e) {
+		const handleKeypress = (e) => {
 			var code = e.keyCode || e.which;
 			if (code !== 13) return;
+			
 			try {
-				var props = self.quoteProps;
-				if (props) {
-					if (e.shiftKey || $('.autocomplete-1TnWNR').length >= 1) return;
-		
-					var messages  = props.messages.filter(m => !m.deleted),
-						guilds    = ReactUtilities.getOwnerInstance($(".guilds-wrapper")[0]).state.guilds.map(o => o.guild),
-						msg		  = props.messages[0],
-						cc        = ReactUtilities.getOwnerInstance($("form")[0]).props.channel,
-						msgC      = props.channel,
-						msgG      = guilds.filter(g => g.id == msgC.guild_id)[0],
-						
-						author    = msg.author,
-						avatarURL = author.getAvatarURL(),
-						color     = parseInt(msg.colorString ? msg.colorString.slice(1) : 'ffffff', 16),
-						msgCnt    = self.MessageParser.parse(cc, $('.channelTextArea-1HTP3C textarea').val()),
-						text      = messages.map(m => m.content).join('\n'),
-						atServer  = msgC.guild_id && msgC.guild_id != cc.guild_id ? ` at ${msgG.name}` : '',
-						chName    = msgC.isDM() ? `@${msgC._getUsers()[0].username}` : msgC.isGroupDM() ? `${msgC.name}` : `#${msgC.name}`;
-					
-					if (self.selectionP) {
-						var start = self.selectionP.start,
-							end = self.selectionP.end;
-						
-						props.messages.forEach((m, i) => {
-							text = '';
-							if(!m.deleted) {
-								var endText = m.content;
-								if(end.index == start.index) endText = m.content.substring(start.offset, end.offset);
-								else if(i == start.index) endText = m.content.substring(start.offset);
-								else if(i == end.index) endText = m.content.substring(0, end.offset);
-								if(i >= start.index && i <= end.index) text += `${endText}\n`;
-							}
-						});
-					}
-					// os dados do embed 
-					let embed = {
-							author: {
-								name: msg.nick || author.username,
-								icon_url: avatarURL.startsWith("https://") ? avatarURL : `https://discordapp.com/${avatarURL}`
-							},
-							url: `${self.quoteURL}guild_id=${msgG.id}&channel_id=${msgC.id}&message_id=${msg.id}`,
-							description: text,
-							footer: {
-								text: `in ${chName}${atServer}`
-							},
-							color,
-							timestamp: msg.timestamp.toISOString()
-						},
-						attachments = messages.map(m => m.attachments).reduce((a, b) => a.concat(b));
-						
-					if (attachments.length >= 1) {
-						// checar se tem alguma imagem na mensagem citada, e adicionar ao embed final
-						var imgAt = attachments.filter(a => a.width);
-						if(imgAt.length >= 1)
-							embed.image = {url: attachments[0].url};
-						
-						// checar se tem algum arquivo na mensagem citada, e adicionar ao embed final
-						var otherAt = attachments.filter(a => !a.width);
-						if (otherAt.length >= 1) {
-							embed.fields = [];
-							otherAt.forEach((at, i) => {
-								var emoji = '📁';
-								if (/(.apk|.appx|.pkg|.deb)$/.test(at.filename)) emoji = '📦';
-								if (/(.jpg|.png|.gif)$/.test(at.filename)) emoji = '🖼';
-								if (/(.zip|.rar|.tar.gz)$/.test(at.filename)) emoji = '📚';
-								if (/(.txt)$/.test(at.filename)) emoji = '📄';
-								
-								embed.fields.push({name: `${self.local.attachment} #${i+1}`, value: `${emoji} [${at.filename}](${at.url})`});
-							});
-						}
-					}
-					
-					// cria uma mensagem com o conteúdo desejado (é necessário pra validar um "id")
-					var msg = self.MessageParser.createMessage(cc.id, msgCnt.content);
-					
-					// adiciona a mensagem a lista de mensagens que serão enviadas
-					self.MessageQueue.enqueue({
-						type: "send",
-						message: {
-							channelId: cc.id,
-							content: msgCnt.content,
-							tts: false,
-							nonce: msg.id,
-							embed
-						}
-					}, function(r) {
-						r.ok ? (self.MessageController.receiveMessage(cc.id, r.body)) : (r.status >= 400 && r.status < 500 && r.body && self.MessageController.sendClydeError(cc.id, r.body.code),
-						self.EventDispatcher.dispatch({
-							type: self.MainDiscord.ActionTypes.MESSAGE_SEND_FAILED,
-							messageId: msg.id,
-							channelId: cc.id
-						}))
-					});
-					
-					ReactUtilities.getOwnerInstance($('form')[0]).setState({textValue: ''});
-					
-					self.cancelQuote();
-					e.preventDefault();
-					e.stopPropagation();
-					return;
-				}
-			} 
-			catch (e) {
-				self.log(e, "warn");
+				if (this.canEmbed())
+					this.sendEmbedQuote(e);
+				else
+					this.sendTextQuote(e);
+			} catch (e) {
+				this.log(e, "warn");
 			}
 		};
-		el[0].addEventListener("keydown", this.handleKeypress, false);
-		el[0].addEventListener("keyup", function(e) {
-			if (e.keyCode == 27 && self.quoteProps)
-				self.cancelQuote();
+		
+		el[0].addEventListener("keydown", handleKeypress, false);
+		el[0].addEventListener("keyup", (e) => {
+			if (e.keyCode == 27 && this.quoteProps) this.cancelQuote();
 		}, false);
+	}
+	
+	sendEmbedQuote(e) {
+		var props = this.quoteProps;
+		if (props) {
+			if (e.shiftKey || $('.autocomplete-1TnWNR').length >= 1) return;
+		
+			var messages  = props.messages.filter(m => !m.deleted),
+				guilds    = this.guilds,
+				msg		  = props.messages[0],
+				cc        = ReactUtilities.getOwnerInstance($("form")[0]).props.channel,
+				msgC      = props.channel,
+				msgG      = guilds.filter(g => g.id == msgC.guild_id)[0],
+				
+				author    = msg.author,
+				avatarURL = author.getAvatarURL(),
+				color     = parseInt(msg.colorString ? msg.colorString.slice(1) : 'ffffff', 16),
+				msgCnt    = this.MessageParser.parse(cc, $('.channelTextArea-1HTP3C textarea').val()),
+				text      = messages.map(m => m.content).join('\n'),
+				atServer  = msgC.guild_id && msgC.guild_id != cc.guild_id ? ` at ${msgG.name}` : '',
+				chName    = msgC.isDM() ? `@${msgC._getUsers()[0].username}` : msgC.isGroupDM() ? `${msgC.name}` : `#${msgC.name}`;
+					
+			if (this.selectionP) {
+				var start = this.selectionP.start,
+					end = this.selectionP.end;
+				
+				props.messages.forEach((m, i) => {
+					text = '';
+					if(!m.deleted) {
+						var endText = m.content;
+						if(end.index == start.index) endText = m.content.substring(start.offset, end.offset);
+						else if(i == start.index) endText = m.content.substring(start.offset);
+						else if(i == end.index) endText = m.content.substring(0, end.offset);
+						if(i >= start.index && i <= end.index) text += `${endText}\n`;
+					}
+				});
+			}
+			// os dados do embed 
+			let embed = {
+					author: {
+						name: msg.nick || author.username,
+						icon_url: avatarURL.startsWith("https://") ? avatarURL : `https://discordapp.com/${avatarURL}`
+					},
+					url: `${this.quoteURL}${msgG ? `guild_id=${msgG.id}&` : ''}channel_id=${msgC.id}&message_id=${msg.id}`,
+					description: text,
+					footer: {
+						text: `in ${chName}${atServer}`
+					},
+					color,
+					timestamp: msg.timestamp.toISOString()
+				},
+				attachments = messages.map(m => m.attachments).reduce((a, b) => a.concat(b));
+						
+			if (attachments.length >= 1) {
+				// checar se tem alguma imagem na mensagem citada, e adicionar ao embed final
+				var imgAt = attachments.filter(a => a.width);
+				if(imgAt.length >= 1)
+					embed.image = {url: attachments[0].url};
+				
+				// checar se tem algum arquivo na mensagem citada, e adicionar ao embed final
+				var otherAt = attachments.filter(a => !a.width);
+				if (otherAt.length >= 1) {
+					embed.fields = [];
+					otherAt.forEach((at, i) => {
+						var emoji = '📁';
+						if (/(.apk|.appx|.pkg|.deb)$/.test(at.filename)) emoji = '📦';
+						if (/(.jpg|.png|.gif)$/.test(at.filename)) emoji = '🖼';
+						if (/(.zip|.rar|.tar.gz)$/.test(at.filename)) emoji = '📚';
+						if (/(.txt)$/.test(at.filename)) emoji = '📄';
+						
+						embed.fields.push({name: `${this.local.attachment} #${i+1}`, value: `${emoji} [${at.filename}](${at.url})`});
+					});
+				}
+			}
+					
+			// cria uma mensagem com o conteúdo desejado (é necessário pra validar um "id")
+			var msg = this.MessageParser.createMessage(cc.id, msgCnt.content);
+				
+			// adiciona a mensagem a lista de mensagens que serão enviadas
+			this.MessageQueue.enqueue({
+				type: "send",
+				message: {
+					channelId: cc.id,
+					content: msgCnt.content,
+					tts: false,
+					nonce: msg.id,
+					embed
+				}
+			}, function(r) {
+				r.ok ? (this.MessageController.receiveMessage(cc.id, r.body)) : (r.status >= 400 && r.status < 500 && r.body && this.MessageController.sendClydeError(cc.id, r.body.code),
+				this.EventDispatcher.dispatch({
+					type: this.MainDiscord.ActionTypes.MESSAGE_SEND_FAILED,
+					messageId: msg.id,
+					channelId: cc.id
+				}))
+			});
+					
+			ReactUtilities.getOwnerInstance($('form')[0]).setState({textValue: ''});
+		
+			this.cancelQuote();
+			e.preventDefault();
+			e.stopPropagation();
+			return;
+		}
+	}
+	
+	sendTextQuote(e) {
+		var props = this.quoteProps;
+		if (props) {
+			if (e.shiftKey || $('.autocomplete-1TnWNR').length >= 1) return;
+		
+			var messages  = props.messages.filter(m => !m.deleted),
+				guilds    = this.guilds,
+				msg		  = props.messages[0],
+				cc        = ReactUtilities.getOwnerInstance($("form")[0]).props.channel,
+				msgC      = props.channel,
+				msgG      = guilds.filter(g => g.id == msgC.guild_id)[0],
+				
+				author    = msg.author,
+				avatarURL = author.getAvatarURL(),
+				color     = parseInt(msg.colorString ? msg.colorString.slice(1) : 'ffffff', 16),
+				content   = this.MessageParser.parse(cc, $('.channelTextArea-1HTP3C textarea').val()).content,
+				text      = messages.map(m => m.content).join('\n'),
+				atServer  = msgC.guild_id && msgC.guild_id != cc.guild_id ? ` at ${msgG.name}` : '',
+				chName    = msgC.isDM() ? `@${msgC._getUsers()[0].username}` : msgC.isGroupDM() ? `${msgC.name}` : `#${msgC.name}`;
+					
+			if (this.selectionP) {
+				var start = this.selectionP.start,
+					end = this.selectionP.end;
+				
+				props.messages.forEach((m, i) => {
+					text = '';
+					if(!m.deleted) {
+						var endText = m.content;
+						if(end.index == start.index) endText = m.content.substring(start.offset, end.offset);
+						else if(i == start.index) endText = m.content.substring(start.offset);
+						else if(i == end.index) endText = m.content.substring(0, end.offset);
+						if(i >= start.index && i <= end.index) text += `${endText}\n`;
+					}
+				});
+			}
+			
+			const format = 'DD-MM-YYYY HH:mm';
+			content     += `\n${'```'}\n${this.MessageParser.unparse(text, cc.id).replace(/\n?(```((\w+)?\n)?)+/g, '\n').trim()}\n${'```'}`;
+			content     += `\`${msg.nick || author.username} - ${this.moment(msg.timestamp).format(format)} | ${chName}${atServer}\``;
+			content      = content.trim();
+					
+			this.MessageController.sendMessage(cc.id, { content });
+					
+			ReactUtilities.getOwnerInstance($('form')[0]).setState({textValue: ''});
+		
+			this.cancelQuote();
+			e.preventDefault();
+			e.stopPropagation();
+			return;
+		}
 	}
 	
 	patchExternalLinks() {
@@ -352,7 +414,10 @@ class Citador {
 				e.preventDefault();
 				const querystring = require('querystring');
 				const {guild_id, channel_id, message_id} = querystring.parse(t.href.substring(self.quoteURL.length));
-				self.HistoryUtils.transitionTo(self.MainDiscord.Routes.MESSAGE(guild_id, channel_id, message_id))
+				if (!guild_id || self.guilds.find(g => g.id == guild_id))
+					self.HistoryUtils.transitionTo(self.MainDiscord.Routes.MESSAGE(guild_id, channel_id, message_id));
+				else
+					ReactUtilities.getOwnerInstance($('.app')[0]).shake();
 			} else 
 				this.oldOnClick(e);
 		}
@@ -401,6 +466,7 @@ class Citador {
 		this.remove("#citador-css");
 		this.switchObserver.disconnect();
 	}
+	get guilds      () { return ReactUtilities.getOwnerInstance($(".guilds-wrapper")[0]).state.guilds.map(o => o.guild)}
 	getName         () { return "Citador";            }
 	getDescription  () { return this.local.description}
 	getVersion      () { return "1.6.7";              }
@@ -412,12 +478,10 @@ class Citador {
 	onChannelSwitch () {
 		if (this.quoteProps) {
 			this.attachParser();
-			var channel  = ReactUtilities.getOwnerInstance($(".messages-wrapper")[0]),
-				canEmbed = channel.props.channel.isPrivate() || channel.can(0x4800, {channelId: channel.props.channel.id});
 			
 			$('.channelTextArea-1HTP3C').prepend(this.quoteMsg);
 			
-			if (!canEmbed) {
+			if (!this.canEmbed()) {
 				$('.quote-msg').find('.citar-btn.hidden:not(.cant-embed)').toggleClass('hidden cant-embed');
 				new PluginTooltip.Tooltip($('.quote-msg').find('.citar-btn'), this.local.noPermTooltip, 'red');
 			} else
